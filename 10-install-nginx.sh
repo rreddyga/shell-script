40633979d90493713974db82859db73d0eeb6bfa
rreddyga <ramanag2299@gmail.com>
2026-01-30 23:47:55 +0530

    conditions

--- 10-install-nginx.sh
+++ 10-install-nginx.sh
@@ -5,8 +5,14 @@
 USERId=$(id -u)
 
 if [ $USERId -ne 0 ]; then
-    echo "please run  this script  with the root user"
+    echo "please run  this script  with the root user access"
     exit 1
 fi
 echo "installing nginx "
 dnf install nginx -y
+
+if [ $? -ne 0]; then
+ echo "Installing nginx... Failure"
+ exit 1
+else
+    echo "Installing nginx... Success"
