3256de4d444aebdcb22d3e041244170f9024ef99
rreddyga <ramanag2299@gmail.com>
2026-01-30 23:12:30 +0530

    conditions

--- 07-special-variables.sh
+++ 07-special-variables.sh
@@ -11,3 +11,5 @@ echo "PID of the script $$"
 sleep 10 
 echo "PID of the recently executed background process $!"
 echo "All args passed to the script $*"
+
+echo "previous command status check $?"
