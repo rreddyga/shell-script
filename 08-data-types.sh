951f3600de47a34f57e396293404b58e19eb76ca
rreddyga <ramanag2299@gmail.com>
2026-01-30 22:48:03 +0530

    data types

---
+++ 08-data-types.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+NUM1=100
+NUM2=300
+SUM=$((NUM1+NUM1))
+
+SUM=$(($NUM1+$NUM1))
+echo "sum of two numbers: $SUM"
+
+# ARRAYS
+
+FRUITS=("Apple" "Banana","Mango","Watermilan")
+echo "fruits are $FRUITS"
