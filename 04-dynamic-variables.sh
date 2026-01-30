e389f7dabd011e870c068a8c9d21e7e42600d44b
rreddyga <ramanag2299@gmail.com>
2026-01-30 22:13:51 +0530

    file name rename

---
+++ 04-dynamic-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1: Hello $PERSON2, how are you? "
+echo "$PERSON2: Hi $PERRSON1,  I am fine  thanks for asking, how are you ?"
+echo "$PERSON1:  doing fine,  what you are learing  $PERSON2?"
+echo "$PERSON2: I'm learning Shell scripting, what about you"
---
+++ 05-readdata-from-user.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Please enter your username:: "
+read USER_NAME
+echo "$USER_NAME"
+
+echo "Please enter your password::"
+read -s PASSWORD
+echo "$PASSWORD"
