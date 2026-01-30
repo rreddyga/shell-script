9fdfba54b22516f3ad580a6470268cf9fe00ebcc
rreddyga <ramanag2299@gmail.com>
2026-01-30 22:49:30 +0530

    data types

--- 08-data-types.sh
+++ 08-data-types.sh
@@ -10,4 +10,7 @@ echo "sum of two numbers: $SUM"
 # ARRAYS
 
 FRUITS=("Apple" "Banana","Mango","Watermilan")
-echo "fruits are $FRUITS"
+echo "fruits are ${FRUITS[@]}"
+echo "fruits are ${FRUITS[0]}"
+echo "fruits are ${FRUITS[1]}"
+echo "fruits are ${FRUITS[2]}"
