5c28b8849d24ac6e91e0e215e5b3f0d293a62ce6
rreddyga <ramanag2299@gmail.com>
2026-01-30 23:00:32 +0530

    conditions

---
+++ 09-conditions.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+NUMBER=$1
+
+# -gt -> grearthan
+# -lt -> lessthan
+# -eq -> equal
+
+if [$NUMBER -gt 20]; then
+    echo "Given number $NUMBER is greater than 20"
+else 
+    echo "Given number $NUMBER is less than 20"
+fi
+
