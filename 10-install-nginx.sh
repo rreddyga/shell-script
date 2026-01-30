d84e0b07d525108aeb36c3a510f673f2768ac2ff
rreddyga <ramanag2299@gmail.com>
2026-01-30 23:26:11 +0530

    conditions

--- 10-install-nginx.sh
+++ 10-install-nginx.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
+# we need to check the root user or not 
+
+USER=$(id -u)
+
+if [ USER -eq 0 ]; then
+    echo "please run  this script  with the root user"
+fi
+
+echo "installing nginx "
 dnf install nginx -y
