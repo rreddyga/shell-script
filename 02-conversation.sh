052a875c24b4af46ec2b9977e3833a6200abedf2
rreddyga <ramanag2299@gmail.com>
2026-01-30 22:01:00 +0530

    file name rename

---
+++ 02-conversation.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Dharsi: Hello Lishanth, how are you? "
+echo "Lishanth: Hi Dharsi,  I am fine  thanks for asking, how are you ?"
+echo "Dharsi:  doing fine,  what you are learing  Lishanth?"
+echo "Lishanth: I'm learning Shell scripting, what about you"
+
+
