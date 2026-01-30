9bc15cf79682e2f511100d26e3b9bd7153ab9641
rreddyga <ramanag2299@gmail.com>
2026-01-30 21:56:51 +0530

    file name rename

---
+++ 01-hello-world.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#  This is the comment, this will not executed by bash, this is the information purpose only
+
+echo "Welcome to Shell Scripting"
+
+echo "I'm learning devops by sivakurmar reddy join devops tutorial"
