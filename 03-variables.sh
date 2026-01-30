bc831b33af0c0a8a7f167a78642d136f1d33fd34
rreddyga <ramanag2299@gmail.com>
2026-01-30 22:06:44 +0530

    file name rename

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1="Dharsi"
+PERSON2="Lishanth"
+
+echo "$PERSON1: Hello $PERSON2, how are you? "
+echo "$PERSON2: Hi $PERRSON1,  I am fine  thanks for asking, how are you ?"
+echo "$PERSON1:  doing fine,  what you are learing  $PERSON2?"
+echo "$PERSON2: I'm learning Shell scripting, what about you"
