e91309fc47695d677c5d56e25d8f2dee0a0ff2a4
rreddyga <ramanag2299@gmail.com>
2026-01-31 19:36:21 +0530

    file added

---
+++ 15-given-package-installed.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# we need to check the root user or not 
+
+USERId=$(id -u)
+LOGS_FOLDER="/var/log/shell-script"
+LOGS_FILE=$LOGS_FOLDER/$0.log
+
+if [ $USERId -ne 0 ]; then
+    echo "please run  this script  with the root user access" | tee -a $LOGS_FILE
+    exit 1
+fi
+VALIDATE()
+{
+    if [ $1 -ne 0 ]; then
+        echo "$2... Failure" | tee -a $LOGS_FILE
+        exit 1
+    else
+        echo "$2... Success" | tee -a $LOGS_FILE
+    fi
+}
+
+for package in $@ # sudo sh 14-loops.sh mysql nodejs gcc
+do
+    dnf list installed  $package &>>LOGS_FILE
+    if [ $? -ne 0 ]; then
+        echo "$package not installed, installing now"
+        dnf install $package -y &>>LOGS_FILE
+        VALIDATE $? "$package installation"
+    else
+        echo "$package  already installed, skipping now"
+    fi
+done
