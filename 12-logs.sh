b453e1a98906e4f83da5e879f978c9015975ee6a
rreddyga <ramanag2299@gmail.com>
2026-01-31 10:36:45 +0530

    updated the files

--- 12-logs.sh
+++ 12-logs.sh
@@ -3,6 +3,8 @@
 # we need to check the root user or not 
 
 USERId=$(id -u)
+LOGS_FOLDER="/var/log/shell-script"
+LOGS_FILE=$LOGS_FOLDER/$0.log
 
 if [ $USERId -ne 0 ]; then
     echo "please run  this script  with the root user access"
@@ -18,13 +20,13 @@ VALIDATE()
     fi
 }
 echo "nginx installing..."
-dnf install nginx -y
+dnf install nginx -y &>> $LOGS_FILE
 VALIDATE $? "Installation nginx "
 
 echo "nodejs installing..."
-dnf install nodejs -y
+dnf install nodejs -y &>> $LOGS_FILE
 VALIDATE $? "Installation nodejs"
 
 echo "mysql installing..."
-dnf install mysql -y
+dnf install mysql -y &>> $LOGS_FILE
 VALIDATE $? "Installation mysql"
--- 13-tee.sh
+++ 13-tee.sh
@@ -3,28 +3,30 @@
 # we need to check the root user or not 
 
 USERId=$(id -u)
+LOGS_FOLDER="/var/log/shell-script"
+LOGS_FILE=$LOGS_FOLDER/$0.log
 
 if [ $USERId -ne 0 ]; then
-    echo "please run  this script  with the root user access"
+    echo "please run  this script  with the root user access" | tee -a $LOGS_FILE
     exit 1
 fi
 VALIDATE()
 {
     if [ $1 -ne 0 ]; then
-        echo "$2... Failure"
+        echo "$2... Failure" | tee -a $LOGS_FILE
         exit 1
     else
-        echo "$2... Success"
+        echo "$2... Success" | tee -a $LOGS_FILE
     fi
 }
 echo "nginx installing..."
-dnf install nginx -y
+dnf install nginx -y &>> $LOGS_FILE
 VALIDATE $? "Installation nginx "
 
 echo "nodejs installing..."
-dnf install nodejs -y
+dnf install nodejs -y &>> $LOGS_FILE
 VALIDATE $? "Installation nodejs"
 
 echo "mysql installing..."
-dnf install mysql -y
+dnf install mysql -y &>> $LOGS_FILE
 VALIDATE $? "Installation mysql"
